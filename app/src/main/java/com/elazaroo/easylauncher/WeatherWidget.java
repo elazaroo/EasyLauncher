0bc32a071b23e96194d6fe30b5144c894255f648
elazaroo <enekolazaroalba@gmail.com>
2025-02-18 09:44:23 +0100

    Weather fix
    Weather fix & Saving location and weather in local storage for skipping unnecessary API CALLS

--- app/src/main/java/com/elazaroo/easylauncher/WeatherWidget.java
+++ app/src/main/java/com/elazaroo/easylauncher/WeatherWidget.java
@@ -3,6 +3,7 @@ package com.elazaroo.easylauncher;
 import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.location.Location;
 import android.net.Uri;
 import android.os.Handler;
@@ -59,22 +60,20 @@ public class WeatherWidget {
 
         Log.d(TAG, "WeatherWidget constructor called");
         requestLocationUpdates();
-        scheduleWeatherUpdates(); // Inicia actualizaciones periódicas
+        scheduleWeatherUpdates();
     }
 
     private void scheduleWeatherUpdates() {
         weatherUpdateHandler.postDelayed(new Runnable() {
             @Override
             public void run() {
-                // Fetch weather data at regular intervals
                 if (lastKnownLatitude != 0.0 && lastKnownLongitude != 0.0) {
                     fetchWeatherData(lastKnownLatitude, lastKnownLongitude);
                 }
 
-                // Schedule the next update in 1 hour (60 * 60 * 1000 ms)
                 weatherUpdateHandler.postDelayed(this, 60 * 60 * 1000);
             }
-        }, 60 * 60 * 1000); // Initial delay for the first update
+        }, 60 * 60 * 1000);
     }
 
 
@@ -91,8 +90,8 @@ public class WeatherWidget {
 
         LocationRequest locationRequest = LocationRequest.create()
                 .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY)
-                .setInterval(60 * 60 * 1000) // Cada 1 hora
-                .setFastestInterval(15 * 60 * 1000); // Mínimo cada 15 minutos
+                .setInterval(60 * 60 * 1000)
+                .setFastestInterval(15 * 60 * 1000);
 
         fusedLocationClient.requestLocationUpdates(locationRequest, locationCallback, Looper.getMainLooper());
     }
@@ -105,7 +104,6 @@ public class WeatherWidget {
                 double newLatitude = location.getLatitude();
                 double newLongitude = location.getLongitude();
 
-                // Solo actualiza si la ubicación ha cambiado significativamente
                 if (Math.abs(newLatitude - lastKnownLatitude) > 0.001 || Math.abs(newLongitude - lastKnownLongitude) > 0.001) {
                     lastKnownLatitude = newLatitude;
                     lastKnownLongitude = newLongitude;
@@ -122,6 +120,11 @@ public class WeatherWidget {
     private void fetchWeatherData(double latitude, double longitude) {
         if (isFetchingWeather) {
             Log.d(TAG, "Weather fetch already in progress, skipping.");
+            return;
+        }
+
+        if (isWeatherDataRecent(latitude, longitude)) {
+            Log.d(TAG, "Weather data is recent, skipping API call.");
             return;
         }
 
@@ -149,7 +152,8 @@ public class WeatherWidget {
                     JSONObject locationJson = new JSONObject(response.body().string());
                     String cityName = locationJson.getString("LocalizedName");
                     String locationKey = locationJson.getString("Key");
-                    fetchCurrentConditions(locationKey, cityName);
+
+                    fetchCurrentConditions(locationKey, cityName, latitude, longitude);
                 } catch (Exception e) {
                     Log.e(TAG, "Failed to parse location key", e);
                 }
@@ -157,7 +161,7 @@ public class WeatherWidget {
         });
     }
 
-    private void fetchCurrentConditions(String locationKey, String cityName) {
+    private void fetchCurrentConditions(String locationKey, String cityName, double latitude, double longitude) {
         String language = Locale.getDefault().getLanguage();
         String weatherUrl = BASE_URL + locationKey + "?apikey=" + API_KEY + "&language=" + language;
         Log.d(TAG, "Fetching weather data from URL: " + weatherUrl);
@@ -177,29 +181,28 @@ public class WeatherWidget {
                 }
 
                 try {
-                    // Convertimos la respuesta en un JSONArray
                     org.json.JSONArray weatherArray = new org.json.JSONArray(response.body().string());
 
                     if (weatherArray.length() > 0) {
                         JSONObject weatherJson = weatherArray.getJSONObject(0);
 
-                        // Extraemos la descripción del clima
                         String description = weatherJson.getString("WeatherText");
                         String uri = weatherJson.getString("MobileLink");
 
-                        // Set onclick to weather_widget to open uri
-                        widgetView.setOnClickListener(v -> {
-                            Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(uri));
-                            context.startActivity(intent);
-                        });
-
-                        // Extraemos y formateamos el icono
                         int iconNumber = weatherJson.getInt("WeatherIcon");
                         String formattedIconNumber = String.format("%02d", iconNumber);
                         String iconUrl = "https://developer.accuweather.com/sites/default/files/" + formattedIconNumber + "-s.png";
 
-                        // Actualizar el widget con la ciudad y el clima
-                        updateWeatherWidget(cityName, description, iconUrl);
+                        new Handler(Looper.getMainLooper()).post(() -> {
+                            updateWeatherWidget(cityName, description, iconUrl);
+
+                            widgetView.setOnClickListener(v -> {
+                                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(uri));
+                                context.startActivity(intent);
+                            });
+
+                            saveLastWeatherData(latitude, longitude, System.currentTimeMillis(), cityName, description, iconUrl);
+                        });
                     } else {
                         Log.e(TAG, "Weather data array is empty");
                     }
@@ -218,5 +221,39 @@ public class WeatherWidget {
             weatherDescription.setText(description);
             Glide.with(context).load(iconUrl).into(weatherIcon);
         });
+    }
+
+    private void saveLastWeatherData(double latitude, double longitude, long timestamp, String cityName, String description, String iconUrl) {
+        SharedPreferences sharedPreferences = context.getSharedPreferences("WeatherWidgetPrefs", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putLong("lastLatitude", Double.doubleToRawLongBits(latitude));
+        editor.putLong("lastLongitude", Double.doubleToRawLongBits(longitude));
+        editor.putLong("lastUpdateTime", timestamp);
+        editor.putString("lastCityName", cityName);
+        editor.putString("lastDescription", description);
+        editor.putString("lastIconUrl", iconUrl);
+        editor.apply();
+    }
+
+    private boolean isWeatherDataRecent(double latitude, double longitude) {
+        SharedPreferences sharedPreferences = context.getSharedPreferences("WeatherWidgetPrefs", Context.MODE_PRIVATE);
+        long lastLatitudeBits = sharedPreferences.getLong("lastLatitude", Double.doubleToRawLongBits(0.0));
+        long lastLongitudeBits = sharedPreferences.getLong("lastLongitude", Double.doubleToRawLongBits(0.0));
+        long lastUpdateTime = sharedPreferences.getLong("lastUpdateTime", 0);
+
+        double lastLatitude = Double.longBitsToDouble(lastLatitudeBits);
+        double lastLongitude = Double.longBitsToDouble(lastLongitudeBits);
+        long currentTime = System.currentTimeMillis();
+
+        boolean isRecent = (Math.abs(latitude - lastLatitude) < 0.001 && Math.abs(longitude - lastLongitude) < 0.001 && (currentTime - lastUpdateTime) < 60 * 60 * 1000);
+
+        if (isRecent) {
+            String cityName = sharedPreferences.getString("lastCityName", "");
+            String description = sharedPreferences.getString("lastDescription", "");
+            String iconUrl = sharedPreferences.getString("lastIconUrl", "");
+            updateWeatherWidget(cityName, description, iconUrl);
+        }
+
+        return isRecent;
     }
 }
